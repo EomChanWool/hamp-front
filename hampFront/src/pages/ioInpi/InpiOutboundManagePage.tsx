b7b619a710e296d6dcad5c88070c0352c14aab6d
taemin <taemin34297@gmail.com>
2026-07-01 15:10:48 +0900

    260701 출고관리 추가

---
+++ hampFront/src/pages/ioInpi/InpiOutboundManagePage.tsx
@@ -0,0 +1,25 @@
+import { mesScreens } from '../../data/mesScreens'
+import { RowDetailModal } from '../../components/RowDetailModal'
+import { SearchBand } from '../../components/common/SearchBand'
+import { TablePanel } from '../../components/table/TablePanel'
+import { useTableData } from '../../hooks/useTableData'
+import { buildTableRows } from '../../utils/buildTableRows'
+import { DashboardCharts } from '../../components/chart/InOutChart'
+
+
+const DEF = mesScreens.inpiOutboundManage
+
+export function InpiOutboundManagePage() {
+  const { filteredRows, setKeyword, modalRow, setModalRow, handleDelete, handleSave } = useTableData(DEF.rows)
+  const fields = DEF.columns.map((label, i) => ({ label, key: `c${i}` }))
+  const tableRows = buildTableRows(filteredRows, DEF.columns.length, setModalRow, handleDelete, DEF.columns)
+  
+  return (
+    <section className="screenStack">
+      <SearchBand filters={DEF.filters} onSearch={setKeyword} onReset={() => setKeyword('')} />
+        <DashboardCharts pageType="inpiOutbound" />
+      <TablePanel title={`${DEF.title} 목록`} headers={[...DEF.columns, '관리']} rows={tableRows} totalCount={filteredRows.length} action="등록" onAction={() => window.alert('mock 동작입니다.')} onRowClick={(i) => setModalRow(filteredRows[i])} />
+      <RowDetailModal isOpen={modalRow !== null} onClose={() => setModalRow(null)} onSave={handleSave} fields={fields} data={modalRow ?? {}} />
+    </section>
+  )
+}
